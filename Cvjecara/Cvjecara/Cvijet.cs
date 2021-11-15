697ea4d0aa9cfc16c9d670248637bed7a4e8b3f4
Ehlimana <ekrupalija1@etf.unsa.ba>
2021-11-15 20:12:02 +0100

    Ubačen gitignore - samo source files

---
+++ Cvjecara/Cvjecara/Cvijet.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cvjecara
+{
+    public class Cvijet
+    {
+        #region Atributi
+
+        Vrsta vrsta;
+        string latinskoIme, boja;
+        DateTime datumBranja;
+        bool sezonsko;
+        int kolicina;
+
+        #endregion
+
+        #region Properties
+
+        public Vrsta Vrsta { get => vrsta; set => vrsta = value; }
+        public string LatinskoIme 
+        { 
+            get => latinskoIme; 
+            set
+            {
+                if (latinskoIme != null)
+                    throw new FormatException("Nemoguće promijeniti latinsko ime cvijeta!");
+
+                if (string.IsNullOrEmpty(value))
+                    throw new FormatException("Latinsko ime ne može biti prazan string!");
+                latinskoIme = value;
+            }
+        }
+        public string Boja 
+        { 
+            get => boja;
+            set
+            {
+                List<string> boje = new List<string>()
+                { "Žuta", "Crvena", "Bijela", "Roza", "Narandžasta" };
+
+                if (!boje.Contains(value))
+                    throw new FormatException("Unijeli ste nepostojeću boju!");
+
+                bool bojeLjiljana = value == "Žuta" || value == "Bijela" || value == "Crvena",
+                    bojeNevena = value == "Žuta",
+                    bojeMargarete = value == "Žuta" || value == "Bijela",
+                    bojeOrhideje = value != "Narandžasta";
+
+                if ((vrsta == Vrsta.Ljiljan && !bojeLjiljana) || (vrsta == Vrsta.Neven && !bojeNevena) ||
+                    (vrsta == Vrsta.Margareta && !bojeMargarete) || (vrsta == Vrsta.Orhideja && !bojeOrhideje))
+                    throw new FormatException("Unijeli ste pogrešnu boju za zadanu vrstu cvijeća!");
+
+                boja = value;
+            }
+        }
+        public DateTime DatumBranja 
+        { 
+            get => datumBranja;
+            set
+            {
+                if (value > DateTime.Now)
+                    throw new FormatException("Datum branja ne može biti u budućnosti!");
+                datumBranja = value;
+            }
+        }
+        public bool Sezonsko { get => sezonsko; set => sezonsko = value; }
+        public int Kolicina 
+        { 
+            get => kolicina;
+            set
+            {
+                if (value < 1)
+                    throw new FormatException("Količina ne može biti manja od 1!");
+                kolicina = value;
+            }
+        }
+
+        #endregion
+
+        #region Konstruktor
+
+        public Cvijet(Vrsta vrsta, string ime, string boja, DateTime datumBranja, int kol)
+        {
+            Vrsta = vrsta;
+            LatinskoIme = ime;
+            Boja = boja;
+            DatumBranja = datumBranja;
+            List<string> sezonskeVrste = new List<string>()
+            { "Neven", "Margareta", "Ljiljan" };
+            Sezonsko = sezonskeVrste.Contains(vrsta.ToString());
+            Kolicina = kol;
+        }
+
+        #endregion
+
+        #region Metode
+
+        public void ProvjeriKrajSezone()
+        {
+            if (!sezonsko)
+                return;
+
+            int pocetakMjesec = 3,
+                krajMjesec = 9;
+
+            int mjesec = DateTime.Now.Month;
+
+            if (mjesec < pocetakMjesec || mjesec > krajMjesec)
+                kolicina = 0;
+        }
+
+        /// <summary>
+        /// Metoda koja se koristi da se odredi koliko je cvijeće svježe.
+        /// Ukoliko je cvijeće ubrano prije manje od 3 dana, ima maksimalnu ocjenu (5.0).
+        /// Za svaki naredni dan svježina cvijeća se smanjuje za sljedeći koeficijent:
+        /// Ruža: 0.2, Neven 0.3, Margareta 0.4, Orhideja 0.5, Ljiljan 0.6.
+        /// Svakog idućeg dana koeficijent se uduplava.
+        /// Potrebno je vratiti ukupnu ocjenu svježine cvijeća kao rezultat rada funkcije.
+        /// Svježina cvijeća ne može biti manja od 0.0.
+        /// </summary>
+        /// <param name="kriterijKlijenta"></param>
+        /// <returns></returns>
+        public double OdrediSvježinuCvijeća()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ Cvjecara/Cvjecara/Cvjećara.cs
@@ -0,0 +1,169 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cvjecara
+{
+    public class Cvjećara
+    {
+        #region Atributi
+
+        List<Cvijet> cvijeće;
+        List<Buket> buketi;
+        List<Mušterija> mušterije;
+        List<Poklon> naručeniPokloni;
+
+        #endregion
+
+        #region Properties
+
+        public List<Cvijet> Cvijeće { get => cvijeće; }
+        public List<Poklon> NaručeniPokloni { get => naručeniPokloni; set => naručeniPokloni = value; }
+        public List<Mušterija> Mušterije { get => mušterije; set => mušterije = value; }
+
+        #endregion
+
+        #region Konstruktor
+
+        public Cvjećara()
+        {
+            cvijeće = new List<Cvijet>();
+            buketi = new List<Buket>();
+            mušterije = new List<Mušterija>();
+            naručeniPokloni = new List<Poklon>();
+        }
+
+        #endregion
+
+        #region Metode
+
+        public void RadSaCvijećem(Cvijet c, int opcija)
+        {
+            if (opcija == 0)
+            {
+                if (c == null)
+                    throw new NullReferenceException("Nemoguće dodati cvijet koji ne postoji!");
+                else if (cvijeće.Contains(c))
+                    throw new InvalidOperationException("Nemoguće dodati cvijet koji već postoji!");
+                else
+                    cvijeće.Add(c);
+            }
+            else if (opcija == 1)
+            {
+                if (c == null)
+                    throw new NullReferenceException("Nemoguće izmijeniti cvijet koji ne postoji!");
+                else if (cvijeće.Find(cvijet => cvijet.LatinskoIme == c.LatinskoIme) == null)
+                    throw new InvalidOperationException("Nemoguće izmijeniti cvijet koji ne postoji!");
+                else
+                {
+                    cvijeće.Remove(cvijeće.Find(cvijet => cvijet.LatinskoIme == c.LatinskoIme));
+                    cvijeće.Add(c);
+                }
+            }
+            else if (opcija == 2)
+            {
+                if (c == null)
+                    throw new NullReferenceException("Nemoguće obrisati cvijet koji ne postoji!");
+                else if (cvijeće.Find(cvijet => cvijet.LatinskoIme == c.LatinskoIme) == null)
+                    throw new InvalidOperationException("Nemoguće obrisati cvijet koji ne postoji!");
+                else
+                {
+                    cvijeće.Remove(cvijeće.Find(cvijet => cvijet.LatinskoIme == c.LatinskoIme));
+                }
+            }
+            else
+                throw new InvalidOperationException("Unijeli ste nepoznatu opciju!");
+        }
+
+        public void DodajBuket(List<Cvijet> cvijeće, List<string> dodaci, Poklon poklon, double cijena)
+        {
+            Buket b = new Buket(cijena);
+            b.DodajPoklon(poklon);
+            foreach (Cvijet c in cvijeće)
+                b.DodajCvijet(c);
+            foreach (string dodatak in dodaci)
+                b.DodajDodatak(dodatak);
+            buketi.Add(b);
+        }
+
+        public bool ObrišiBuket(Buket b)
+        {
+            return buketi.Remove(b);
+        }
+
+        public List<Buket> DajSveBukete()
+        {
+            return buketi;
+        }
+
+        public void IzvršiNabavku(string godišnjeDoba, string veličinaNarudžbe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PregledajCvijeće()
+        {
+            foreach (Cvijet cvijet in cvijeće)
+            {
+                cvijet.ProvjeriKrajSezone();
+                if (cvijet.OdrediSvježinuCvijeća() < 2)
+                    cvijet.Kolicina = 0;
+            }
+
+            cvijeće.RemoveAll(cvijet => cvijet.Kolicina == 0);
+        }
+
+        public void NaručiCvijeće(Mušterija m, Buket b, Poklon p, Poklon nagrada = null)
+        {
+            if (!buketi.Contains(b))
+                throw new InvalidOperationException("Traženi buket nije na stanju!");
+
+            m.RegistrujKupovinu(b, p);
+            naručeniPokloni.Add(p);
+
+            if (nagrada != null)
+                m.NagradnaKupovina(p);
+        }
+
+        public void ProvjeriLatinskaImenaCvijeća(ILeksikon leksikon)
+        {
+            List<Cvijet> zaObrisati = new List<Cvijet>();
+            foreach (Cvijet c in cvijeće)
+            {
+                if (!leksikon.ValidnoLatinskoIme(c.LatinskoIme))
+                    zaObrisati.Add(c);
+            }
+            cvijeće.RemoveAll(cvijet => zaObrisati.Contains(cvijet));
+        }
+
+        public List<Poklon> DajSveNaručenePoklone(Mušterija m, double popust)
+        {
+            List<Poklon> pokloni = m.KupljeniPokloni.FindAll(poklon => poklon.PostotakPopusta == popust);
+            if (pokloni == null)
+                throw new FormatException("Došlo je do greške! Pokušajte ponovo sa drugim parametrima zahtjeva.");
+
+            return pokloni;
+        }
+
+        /// <summary>
+        /// Metoda koja vraća mušteriju koja je izvršila najveći broj kupovina.
+        /// Ukoliko cvjećara nema nijednu mušteriju, potrebno je baciti izuzetak.
+        /// U suprotnom, potrebno je pronaći mušteriju koja je ukupno kupila najviše cvijeća
+        /// u svim buketima koje je naručila.
+        /// Ukoliko postoji više takvih mušterija, potrebno je vratiti onu mušteriju
+        /// koja je potrošila veći ukupni iznos novca na sve kupljene bukete.
+        /// Ukoliko i u tom slučaju postoji više mušterija, potrebno je baciti izuzetak
+        /// jer se najbolja mušterija u tom slučaju ne može tačno odrediti.
+        /// </summary>
+        /// <returns></returns>
+        public Mušterija DajNajboljuMušteriju()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}

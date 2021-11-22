b178b9c37f574c4c15e5908b8419ea7ae18afcfb
Medina <mkazic1@etf.unsa.ba>
2021-11-22 19:22:12 +0100

    Dodan test za klasu Poklon

--- Cvjecara/TestProject1/PoklonTest.cs
+++ Cvjecara/TestProject1/PoklonTest.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Cvjecara;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace TestProject1
 {
     [TestClass]
     public class PoklonTest
     {
+
+    /// <summary>
+    /// Test provjere postotka
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void TestMethod1()
+    {
+        //Radila Medina
+        Poklon poklončić = new("Loptica", 0);
+    }
     }
 }

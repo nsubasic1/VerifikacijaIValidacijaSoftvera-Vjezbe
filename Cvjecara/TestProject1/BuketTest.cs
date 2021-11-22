fbc3aa7455b2dd6b6fba1dd72b6d4bb39baf0a84
Medina <mkazic1@etf.unsa.ba>
2021-11-22 19:28:45 +0100

    Dodan test za klasu Buket

--- Cvjecara/TestProject1/BuketTest.cs
+++ Cvjecara/TestProject1/BuketTest.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Cvjecara;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace TestProject1
 {
     [TestClass]
     public class BuketTest
     {
-        
+        /// <summary>
+        /// Test izuzetka
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void TestMethod1()
+        {
+            //Radila Medina
+            Buket buketic= new(0.0003);
+        }
+
     }
 }

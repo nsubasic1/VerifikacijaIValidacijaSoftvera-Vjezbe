13bf2a156e227593dbccca2724f025af5e47689d
Medina <mkazic1@etf.unsa.ba>
2022-01-03 17:54:08 +0100

    Dodan MSTest projekat

--- Cvjecara/Cvjecara/Cvjećara.cs
+++ Cvjecara/Cvjecara/Cvjećara.cs
@@ -285,24 +285,4 @@ namespace Cvjecara
 
         #endregion
     }
-
-    [TestClass]
-    public class CvjećaraTest
-    {
-        [TestMethod]
-        public void TestTuning()
-        {
-            Cvjećara klasa = new Cvjećara();
-            for (int i = 0; i < 10000000; i++)
-                klasa.Cvijeće.Add(new Cvijet(Vrsta.Ruža, "Rosa", "Crvena", DateTime.Now.AddDays(-1), 1));
-            Cvijet c = new Cvijet(Vrsta.Orhideja, "Orchid", "Crvena", DateTime.Now.AddDays(-1), 1);
-            //prvi breakpoint prije poziva metode
-            int x = 0;
-            //prije tuninga
-            klasa.RadSaCvijećem(c, 0, 1);
-            int y = 0;
-
-            Assert.IsTrue(true);
-        }
-    }
 }
---
+++ Cvjecara/TestProject/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Cvjecara;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public void TestTuning()
+        {
+            Cvjećara klasa = new Cvjećara();
+            for (int i = 0; i < 10000000; i++)
+                klasa.Cvijeće.Add(new Cvijet(Vrsta.Ruža, "Rosa", "Crvena", DateTime.Now.AddDays(-1), 1));
+            Cvijet c = new Cvijet(Vrsta.Orhideja, "Orchid", "Crvena", DateTime.Now.AddDays(-1), 1);
+            //prvi breakpoint prije poziva metode
+            int x = 0;
+            //prije tuninga
+            klasa.RadSaCvijećem(c, 0, 1);
+            int y = 0;
+
+            Assert.IsTrue(true);
+        }
+    }
+}

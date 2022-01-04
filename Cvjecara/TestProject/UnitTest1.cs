c6b113ddddcd08a2757378a581a4074c73c3b345
Medina <mkazic1@etf.unsa.ba>
2022-01-04 12:10:10 +0100

    Promjenjen Diagnostic

--- Cvjecara/TestProject/UnitTest1.cs
+++ Cvjecara/TestProject/UnitTest1.cs
@@ -19,35 +19,10 @@ namespace TestProject
             for (int i = 0; i < 100; i++)
             {
                 Cvijet c = new Cvijet(Vrsta.Orhideja, "Orchid", "Crvena", DateTime.Now.AddDays(-1), 1);
-                klasa.RadSaCvijećem(c, 0, 1);
-                klasa.RadSaCvijećem(c, 2, 1);
-            }
-            int y = 0;
-
-            for (int i = 0; i < 100; i++)
-            {
-                Cvijet c = new Cvijet(Vrsta.Orhideja, "Orchid", "Crvena", DateTime.Now.AddDays(-1), 1);
-                klasa.RadSaCvijećemTuning1(c, 0, 1);
-                klasa.RadSaCvijećemTuning1(c, 2, 1);
-            }
-            int z = 0;
-
-            for (int i = 0; i < 100; i++)
-            {
-                Cvijet c = new Cvijet(Vrsta.Orhideja, "Orchid", "Crvena", DateTime.Now.AddDays(-1), 1);
-                klasa.RadSaCvijećemTuning2(c, 0, 1);
-                klasa.RadSaCvijećemTuning2(c, 2, 1);
-            }
-            int l = 0;
-
-            for (int i = 0; i < 100; i++)
-            {
-                Cvijet c = new Cvijet(Vrsta.Orhideja, "Orchid", "Crvena", DateTime.Now.AddDays(-1), 1);
                 klasa.RadSaCvijećemTuning3(c, 0, 1);
                 klasa.RadSaCvijećemTuning3(c, 2, 1);
             }
-            int k = 0;
-
+            int y = 0;
 
             Assert.IsTrue(true);
         }
